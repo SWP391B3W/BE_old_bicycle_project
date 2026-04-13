35da95945ff17ee53bd970dfafe4089b8e152cc4
Hoa02092004 <hoak027@gmail.com>
2026-04-13 13:38:19 +0700

    Initial commit: Old Bicycle Project backend source code

---
+++ old_bicycle_project/src/main/java/swp391/old_bicycle_project/OldBicycleProjectApplication.java
@@ -0,0 +1,13 @@
+package swp391.old_bicycle_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldBicycleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldBicycleProjectApplication.class, args);
+	}
+
+}
---
+++ old_bicycle_project/src/test/java/swp391/old_bicycle_project/OldBicycleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package swp391.old_bicycle_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldBicycleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
